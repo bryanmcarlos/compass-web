bd9faded9a93da340b617276d6f3053479317ed8
Bryan Carlos <bryanmcarlos@gmail.com>
2026-07-21 23:47:10 +0400

    Update Layout.tsx

--- src/components/club/Layout.tsx
+++ src/components/club/Layout.tsx
@@ -15,7 +15,7 @@ export function Layout({
       <Sidebar isAdmin={isAdmin} isMarshalOrAdmin={isMarshalOrAdmin} />
 
       <main className="pb-[calc(4rem+env(safe-area-inset-bottom))] lg:pb-0 lg:pl-64">
-        <div className="mx-auto grid max-w-5xl gap-6 px-4 py-6 sm:px-6 lg:px-8 lg:py-10">
+        <div className="mx-auto grid grid-cols-1 max-w-5xl gap-6 px-4 py-6 sm:px-6 lg:px-8 lg:py-10">
           {children}
         </div>
       </main>
